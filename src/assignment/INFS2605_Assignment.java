86eb95c942471bcf53f3e33d5ab55695f57fe0a9
SONY <SONY@10.248.84.199>
2015-04-27 06:34:10 +1000

    Hello

--- src/assignment/INFS2605_Assignment.java
+++ src/assignment/INFS2605_Assignment.java
@@ -16,7 +16,9 @@ public class INFS2605_Assignment {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Hello
+        
+// TODO code application logic here
     }
     
 }
